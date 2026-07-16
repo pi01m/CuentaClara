e88ce1632f9b3408a42f614a2835c4207b36ecb2
Pilar Olmedo <pilarolmedo265@gmail.com>
2026-07-16 19:38:25 -0300

    .

--- BLL/BLL_DigitoVerificador.cs
+++ BLL/BLL_DigitoVerificador.cs
@@ -135,7 +135,13 @@ namespace BLL
         public void ValidarTodaLaBase()
         {
             List<ExcepcionIntegridad> errores = new List<ExcepcionIntegridad>();
-            
+            // ---------- Idioma ----------
+            var errorIdioma = ValidarIntegridad(
+                dalIdioma.DameIdiomasBD(),
+                "Idioma");
+
+            if (errorIdioma != null)
+                errores.Add(errorIdioma);
 
             // ---------- Permiso ----------
             var errorPermiso = ValidarIntegridad(
@@ -249,7 +255,7 @@ namespace BLL
             RecalcularUsuarios(log);
             RecalcularRoles(log);
             RecalcularFamilias(log);
-           
+            RecalcularIdiomas(log);
             RecalcularPermisos(log);
         }
 
@@ -442,7 +448,48 @@ namespace BLL
         }
 
 
-        
+        private void RecalcularIdiomas(string log)
+        {
+            dalDigito.EliminarDVHDeTabla("Idioma");
+
+            List<Servicio_Idioma> idiomas =
+                dalIdioma.DameIdiomasBD()
+                         .OrderBy(i => i.ObtenerIdentificadorFila())
+                         .ToList();
+
+            string cadenaDVV = "";
+
+            foreach (Servicio_Idioma idioma in idiomas)
+            {
+                string dvh = servicioCalcular.CalcularDVH(idioma);
+
+                Servicio_DigitoVerificadorVertical reg =
+                    new Servicio_DigitoVerificadorVertical();
+
+                reg.Nombre = "Idioma_" + idioma.ObtenerIdentificadorFila();
+                reg.DVH = dvh;
+
+                dalDigito.GuardarDVH(reg);
+
+                cadenaDVV += dvh;
+            }
+
+            string dvv = servicioCalcular.CalcularHash(cadenaDVV);
+            
+            Servicio_DigitoVerificadorVertical maestro =
+                new Servicio_DigitoVerificadorVertical();
+
+            maestro.Nombre = "Idioma_MAESTRO";
+            maestro.DVV = dvv;
+
+            dalDigito.GuardarDVV(maestro);
+
+            bllBitacora.RegistrarBitacora(
+                "Recalculo de Dígitos Verificadores de Idiomas",
+                log,
+                "Seguridad",
+                1);
+        }
 
 
     }
